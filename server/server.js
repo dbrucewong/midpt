66c6a8ab46d13bff6c4f102b90634afcbe32d496
Bruce Wong <dbrucewong@gmail.com>
2019-06-19 17:30:45 -0700

    Sixth try

--- server/server.js
+++ server/server.js
@@ -36,5 +36,5 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-app.listen(3000); //listens on port 3000 -> http://localhost:3000/
+app.listen(process.env.PORT || 3000); //listens on port 3000 -> http://localhost:3000/
 console.log('this works, at least !');
